f840ad4614be8a195db64177d9259407c1cd9d15
trtd56 <hoge@fuga.com>
2017-08-07 22:05:00 +0900

    update log trace function

--- log_tracer.py
+++ log_tracer.py
@@ -4,21 +4,39 @@ from datetime import datetime
 
 class LogTracer():
 
-    def __init__(self, path="./data/log/"):
-        now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S.csv")
-        self.path = path + "/" + now + "_train_log.csv"
+    def __init__(self, nn_type, sep_mode, path="./data/log/"):
+        now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+        self.path_t = path + "/" + "train_log_" + now + ".csv"
         log = "{}.{},{},{}".format("time", "epoch", "loss", "acc")
-        with open(self.path, "w") as f:
+        with open(self.path_t, "w") as f:
             f.write(log)
+        self.path_v = path + "/" + "test_log_" + now + ".csv"
+        log = "{}.{},{},{},{},{}".format("time", "epoch", "loss_avg", "acc_avg", "loss_v", "acc_v")
+        with open(self.path_v, "w") as f:
+            f.write(log)
+        self.train_loss = []
+        self.train_acc = []
 
-    def trace(self, out):
-        now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-        print("{}\t{}".format(now, out))
-
-    def __call__(self, epoch, loss, acc, trace=False):
-        now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    def trace_train(self, epoch, loss, acc):
+        now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
         log = "\n{},{},{},{}".format(now, epoch, loss, acc)
-        with open(self.path, "a") as f:
+        with open(self.path_t, "a") as f:
+            f.write(log)
+        self.train_loss.append(loss)
+        self.train_acc.append(acc)
+
+    def trace_test(self, epoch, loss, acc, trace=False):
+        now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+        loss_t = sum(self.train_loss)/len(self.train_loss)
+        acc_t = sum(self.train_acc)/len(self.train_acc)
+        log = "\n{},{},{},{},{},{}".format(now, epoch, loss_t, acc_t, loss, acc)
+        with open(self.path_v, "a") as f:
             f.write(log)
         if trace:
-            print("{}\t{}\t{}\t{}".format(now, epoch, loss, acc))
+            print("{}\t{}\t{}\t{}\t{}\t{}".format(now, epoch, loss_t, acc_t, loss, acc))
+        self.train_loss = []
+        self.train_acc = []
+
+    def __call__(self, out):
+        now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+        print("{}\t{}".format(now, out))
